63f95a2b4653cf0e5310cbc04547ffeff992ef71
l0uvre <bladwinw@gmail.com>
2018-08-09 22:33:56 +0800

    2 leetcode problems and more.

---
+++ Java/Data Structure and Algorithms/Quick.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Quick {
+
+  public void sort(int[] arr) {
+    sort(arr, 0, arr.length - 1);
+  }
+
+  public void sort(int[] arr, int begin, int end) {
+    if (begin >= end) {
+      return;
+    }
+    int mid = partition(arr, begin, end);
+    sort(arr, begin, mid - 1);
+    sort(arr, mid + 1, end);
+  }
+
+  public int partition(int[] arr, int begin, int end) {
+    int pivot = arr[begin];
+    int i = begin;
+    int j = end + 1;
+    while (true) {
+      do {
+        i++;
+        if (i > end) {
+          break;
+        }
+      } while (arr[i] < pivot);
+
+      do {
+        j--;
+        if (j < begin) {
+          break;
+        }
+      } while (arr[j] > pivot);
+
+      if (i >= j) {
+        break;
+      }
+
+      exchange(arr, i, j);
+    }
+
+    exchange(arr, begin, j);
+    return j;
+  }
+
+  private void exchange(int[] arr, int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  public void show(int[] arr) {
+    if (arr != null && arr.length > 0) {
+      for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]);
+        if (i == arr.length - 1) {
+          System.out.println();
+        } else {
+          System.out.print("  ");
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Quick quickSort = new Quick();
+    int[] a = {0, 0, 0, 0, 0, 0, 0, 0};
+    quickSort.sort(a);
+    quickSort.show(a);
+    int[] b = {0, 2, 6, 8, 7, 3, 9, 4, 5, 6, 8, 1, 2, 3, 1, 52, 34, 15, 17, 35, 97};
+    quickSort.sort(b);
+    quickSort.show(b);
+  }
+
+}
---
+++ Java/Data Structure and Algorithms/UnionFind.java
@@ -0,0 +1,67 @@
+public class UnionFind {
+    private int[] id;
+    private int[] size;
+    private int N;
+    
+    public UnionFind(int n) {
+        id = new int[n];
+        size = new int[n];
+        N = n;
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    public int find(int i) {
+        while (id[i] != i) {
+            i = id[i];
+        }
+        return i;
+    }
+    
+    public void union(int i, int j) {
+        int id1 = find(i);
+        int id2 = find(j);
+        if (id1 == id2) {
+          return;
+        } 
+        if (size[id1] > size[id2]) {
+            id[id2] = id1;
+            size[id1] += size[id2];
+        } else {
+            id[id1] = id2;
+            size[id2] += size[id1];
+        }
+        N--;
+    }
+
+    public boolean connected(int i, int j) {
+       return find(i) == find(j);
+    }
+
+    public int count() {
+      return N;
+    }
+    
+    public int maxUnionSize() {
+        int res = 0;
+        for (int i = 0; i < size.length; i++) {
+            res = Math.max(size[i], res);
+        }
+        
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        UnionFind uf = new UnionFind(10);
+        uf.union(1, 3);
+        uf.union(6, 9);
+        System.out.println("# components: " + uf.count());
+        uf.union(7, 9);
+        System.out.println("Max component size: " + uf.maxUnionSize());
+
+}
+    
+}
