d8fef2cf186bc6a3ef4e1a7e614e94d74b72545b
l0uvre <bladwinw@gmail.com>
2021-11-09 03:08:17 -0500

    re-code two lc problems

--- Java/LeetCode/Binary Search/FindMinRotated.java
+++ Java/LeetCode/Binary Search/FindMinRotated.java
@@ -1,6 +1,6 @@
 // Leetcode 153 Binary Search 
 public class FindMinRotated {
-
+    /**
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) {
             return -1;
@@ -28,6 +28,24 @@ public class FindMinRotated {
             }
             return nums[mid];
         }
+    }
+    **/
+    /** More consice way Nov. 9 2021
+     *
+     */
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+        return nums[left];
     }
     
     public static void main(String[] args) {
--- Java/LeetCode/Binary Search/FindMinRotatedII.java
+++ Java/LeetCode/Binary Search/FindMinRotatedII.java
@@ -1,6 +1,6 @@
 // Leetcode 154 Binary Search 
 public class FindMinRotatedII {
-
+    /***
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) {
             return -1;
@@ -31,6 +31,34 @@ public class FindMinRotatedII {
             return nums[mid];
         }
     }
+    **/
+
+    /**
+     * On Nov. 9 2021 more concise.
+     */
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if (nums[mid] == nums[right]) {
+                if (nums[left] < nums[right]) {
+                    right = mid - 1;
+                } else if (nums[left] > nums[right]) {
+                    right = mid;
+                } else {
+                    right--; //skip the duplicate
+                }
+            } else {
+                right = mid;
+            }
+
+        }
+        return nums[left];
+    }
+    
 
     public static void main(String[] args) {
         FindMinRotatedII fmri = new FindMinRotatedII();
@@ -38,7 +66,16 @@ public class FindMinRotatedII {
         System.out.println(fmri.findMin(nums));
         nums = new int[]{2, 2, 2, 0, 1};
         System.out.println(fmri.findMin(nums));
+        nums = new int[]{3, 3, 1, 3};
+        System.out.println(fmri.findMin(nums));
     
+        nums = new int[]{3, 3, 3, 3};
+        System.out.println(fmri.findMin(nums));
+        
+        nums = new int[]{3, 3};
+        System.out.println(fmri.findMin(nums));
+        nums = new int[]{1,1,3,1};
+        System.out.println(fmri.findMin(nums));
     }
 
 }
