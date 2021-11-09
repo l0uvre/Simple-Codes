988164620a45792a07df2354995789bcf776587f
l0uvre <bladwinw@gmail.com>
2021-11-08 22:11:05 -0500

    finished LC48 in another way.

--- Java/LeetCode/Array/Rotate.java
+++ Java/LeetCode/Array/Rotate.java
@@ -1,4 +1,4 @@
-// LC 48
+// LC 48 
 public class Rotate {
 
     public void rotate(int[][] matrix) {
@@ -6,14 +6,29 @@ public class Rotate {
             return;
         }
         int n = matrix.length;
+        /**
+         * Reverse the rows
+         */
         for (int i = 0; i < n / 2; i++) {
             int[] temp = matrix[i];
             matrix[i] = matrix[n - 1 - i];
             matrix[n - 1 - i] = temp;
         }
 
+        /**
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }***/
+        
+        /**
+         * swap the elements diagonally
+         */
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < j; i++) {
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
